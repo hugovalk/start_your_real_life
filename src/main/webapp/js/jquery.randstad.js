64e251d26b174ebec235ad89c70a7c39282c5486
Erik Blanken <erik.blanken@randstadgroep.nl>
2015-01-24 13:34:56 +0100

    Full styles for results

--- src/main/webapp/js/jquery.randstad.js
+++ src/main/webapp/js/jquery.randstad.js
@@ -6,18 +6,42 @@
 		
 		//hide the [data-results]-div. 
 		$(this).find("[data-results]").each(function(){
-//			$(this).hide(); 
+			$(this).hide(); 
 		});
 		
 		$(this).find("[data-match]").each(function(){
 			$(this).on('click', function(){
 				
-				//1. Fetch results based on input
+				$("body").waitMe({
+					effect: 'bounce', 
+					text: 'Een moment a.u.b. We zijn voor nu je perspectief aan het berekenen.',
+					bg: 'rgba(255, 255, 255, 0.7)', color: '#000'
+				});
 				
-				//2. Render results
+				//1. Fetch results based on input
+				var serviceUrl="/services/advies"
+					
+				$.ajax({
+					type: "GET", 
+					url: serviceUrl, 
+					data: $document.find("form").first().serialize(),
+					success: function(result){
+						console.log(result);
+					}
+				});	
 				
 				//3. Show the results div via fade in.
 				$document.find("[data-results]").toggle("fold",  {horizFirst: false });
+				
+				//delay force.
+				setTimeout( function() { $("body").waitMe('hide');  }, 500);
+				
+//				$("body").waitMe('hide');
+				
+				
+				//2. Render results
+				
+				
 			});
 		});
 	});
